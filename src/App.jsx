2c9efef9fa849fa918d60c55fe532c79dd037119
sureshjoshi88 <sj3795789@gmail.com>
2025-06-17 11:37:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Weather from './component/Weather'
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+   <Weather/>
+   </>
+  )
+}
+
+export default App
---
+++ src/component/Weather.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+
+const Weather = () => {
+const apiKey = import.meta.env.VITE_API_KEY
+const [cityname,setCityName] = useState('');
+const [data,setData] = useState(null)
+const [errorMsg, setErrorMsg] = useState('');
+
+
+
+const getweather = ()=>{
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apiKey}&units=metric`;
+      fetch(url)
+      .then((res)=>res.json())
+      .then((response)=>{
+        if(response.cod===200){
+          setData(response)
+          setErrorMsg('');
+        }else{
+          setData(null);
+          setErrorMsg(response.message);
+        }
+      })
+      .catch ((error)=>{
+      console.log(error,'somthing went wrong please try again');
+      setErrorMsg(error,'something went wrong')
+      setData(null);
+
+    })
+}
+  
+
+  const submitButton=()=>{
+    if(cityname.trim()===''){
+      alert('please enter a value')
+    }else{
+      setCityName('')
+      getweather()
+    }
+  }
+  
+  const keypressEvent = (e) =>{
+    if(e.key==="Enter"){
+      getweather();
+      setCityName("");
+    }
+  }
+
+  return (
+    <div className='img'>
+      <p className='text-center font-medium text-2xl p-3'>Weather-app</p>
+      <div  className='flex justify-center mt-6'>
+      <div className='bg-white p-2 rounded'>
+         <div className='flex gap-2'>
+        <input type="search" className='border rounded h-8 w-70 p-1' onKeyDown={keypressEvent} value={cityname} onChange={(e)=>setCityName(e.target.value)} name="" id="input-wather" placeholder='Enter a CityName' />
+        <button className='bg-green-800 h-8 rounded p-1 text-white cursor-pointer' onClick={submitButton}>Get Weather</button>
+      </div>
+
+        {errorMsg && (
+            <p className='text-red-600 font-medium text-sm mb-2'>{errorMsg}</p>
+          )}
+        {data && (
+                 <div className='space-y-1'>
+              <p className='font-medium text-lg'>🌡 Temp: {data.main.temp} °C</p>
+              <p className='font-medium text-lg'>💧 Humidity: {data.main.humidity} %</p>
+              <p className='font-medium text-lg'>🌬 Air speed: {data.wind.speed} m/s</p>
+              <p className='font-medium text-lg'>📍 Lat: {data.coord.lat}</p>
+              <p className='font-medium text-lg'>📍 Lon: {data.coord.lon}</p>
+              <p className='font-medium text-lg'>🏙 City: {data.name}</p>
+            </div>
+        )
+      }
+      <p id='error'></p>
+      </div>
+      </div>
+
+    
+    </div>
+  )
+}
+
+export default Weather
